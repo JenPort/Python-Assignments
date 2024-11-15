51d686c9671e43a5d00121418d9284701395c52f
Jenesis <40256583+JenPort@users.noreply.github.com>
2024-11-14 21:12:03 -0500

    Add files via upload

---
+++ JenesisAssignment2.py
@@ -0,0 +1,12 @@
+# Display the length and width of the rectangle
+
+Length = float(input("Enter the Length of the rectangle, must be positive numbers only:"))
+
+Width = float(input("Enter the Width of the rectangle, must be positive Numbers only:"))
+
+# Calculate the Area of the rectangle
+
+Area = (Length * Width)
+
+# Display the calculated Area of the rectangle
+print("The area of the rectangle is:", Area)
